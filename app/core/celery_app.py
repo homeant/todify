39dc8fd980024b25cdb31638216258edf52bb3e5
Huang Tianhui <huangtianhuitian@xiaobangtouzi.com>
2025-01-02 23:33:21 +0800

    feat(celery): 调整每日股票数据抓取时间为23:30

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -29,7 +29,7 @@ def init_celery_app() -> Celery:
     celery_app.conf.beat_schedule = {
         "fetch_daily_stock_data": {
             "task": "app.tasks.stock_tasks.fetch_daily_stock_data",
-            "schedule": crontab(hour="23", minute="32"),
+            "schedule": crontab(hour="23", minute="35"),
             # "kwargs": ({"start_date": "20240701", "end_date": "20241229"}),
         },
         # "calculate_indicators_task": {
